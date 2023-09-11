cb5de88d0b660c9b7a1f06e8d0b3300bc4575b49
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-09-11 16:12:22 +0530

    Time: 2 ms (75.08%) | Memory: 6.1 MB (27.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -9,7 +9,13 @@ private:
     }
 public:
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n,dp);
+        vector<int> dp(n+2,-1);
+        // return helper(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
