7f5392e09215d7c4cfa9afec925640df0f03c24d
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-09-11 16:08:18 +0530

    Time: 10 ms (47.96%) | Memory: 6 MB (27.84%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+private:
+    int helper(int n){
+        if(n<=1) return n;
+        return helper(n-1)+helper(n-2);
+    }
+public:
+    int fib(int n) {
+        return helper(n);
+    }
+};
