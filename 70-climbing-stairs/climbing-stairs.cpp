ab12a3c2a3adc902cdd57ac1926d2b382a99022c
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-09-11 16:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (44.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,14 +4,13 @@ public:
         if(n==0||n==1){
       return 1;
   }
-  int prev2 = 1;
-  int prev = 1;
+  vector<int > dp(n+1,-1);
+  dp[0]=1;
+  dp[1]=1;
   
   for(int i=2; i<=n; i++){
-      int cur_i = prev2+ prev;
-      prev2 = prev;
-      prev= cur_i;
+      dp[i]=dp[i-1]+dp[i-2];
   }
-  return prev;
+  return dp[n];
     }
 };
