52d2d5944fe42a47dddf0f1ede57b6653368a10e
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-09-12 11:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (23.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -14,6 +14,15 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n+1,-1);
-        return helper(n-1,nums,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int non_pick=dp[i-1];
+            dp[i]=max(pick,non_pick);
+        }
+        return dp[n-1];
     }
 };
