928bcfb22d48859ea45e8adb7e364105e863cf37
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-10-08 10:59:00 +0530

    Time: 321 ms (27.73%) | Memory: 153.5 MB (44.04%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -22,7 +22,26 @@ private:
     }
 public:
     long long maxAlternatingSum(vector<int>& nums) {
-        vector<vector<long long >> dp(nums.size()+1,vector<long long >(2,-1));
-        return helper(0,nums,true,dp);
+        vector<vector<long long >> dp(nums.size()+1,vector<long long >(2,0));
+        //base case
+        int n=nums.size();
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int index=n-1;index>=0;index--){
+            for(int j=0;j<=1;j++){
+                //not_pick
+                long long not_pick=dp[index+1][j];
+                //pick
+                long long pick=INT_MIN;
+                if(j){
+                    pick=nums[index]+dp[index+1][!j];
+                }else if(j==false){
+                    pick=-nums[index]+dp[index+1][!j];
+                }
+
+                dp[index][j]=max(not_pick,pick);
+            }
+        }
+        return dp[0][1];
     }
 };
