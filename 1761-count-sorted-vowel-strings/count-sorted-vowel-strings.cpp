26fa91cc670ebc19712dcaa07d115620bea539de
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2023-10-08 17:10:09 +0530

    Time: 2 ms (59.36%) | Memory: 6.4 MB (31.39%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    int dp[51][6];
     int helper(int ind,int n,int vowelInd){
         if(vowelInd==4){
             return 1;
@@ -7,15 +8,19 @@ private:
         if(ind==n){
             return 1;
         }
+        if(dp[ind][vowelInd]!=-1){
+            return dp[ind][vowelInd];
+        }
         //let us pick the vowel
         int pick=helper(ind+1,n,vowelInd);
         //not pick
         int notpick=helper(ind,n,vowelInd+1);
 
-        return pick+notpick;
+        return dp[ind][vowelInd]=pick+notpick;
     }
 public:
     int countVowelStrings(int n) {
+        memset(dp,-1,sizeof(dp));
         return helper(0,n,0);
     }
 };
