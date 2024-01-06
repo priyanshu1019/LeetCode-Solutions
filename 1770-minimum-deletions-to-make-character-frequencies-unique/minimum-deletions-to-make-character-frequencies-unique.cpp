cf14c50fec03d95cd0093470780cedeacdc99c21
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-01-06 19:26:11 +0530

    Time: 53 ms (74.05%) | Memory: 17.7 MB (50.68%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int minDeletions(string s) {
+        
         int minDel = 0;
-        vector<int> freq(26 ,0);
+        vector<int> freq(26 , 0);
         unordered_set<int> st;
-
-        for( auto it:s)
+        for(auto it:s)
         {
             freq[it-'a']++;
         }
-
-        for(auto it : freq)
+        for(auto it:freq)
         {
-            if( it == 0 ) continue;
-            if( st.find(it)!= st.end())
+            if( it == 0) continue;
+            if( st.find(it) != st.end() )
             {
-                while( st.find(it)!= st.end() and it > 0 )
+                while(st.find(it) != st.end() and it > 0 )
                 {
                     it--;
                     minDel++;
+
                 }
-                if( it != 0) st.insert(it);
+                if( it != 0)
+                {
+                    st.insert(it);
+                }
             }else{
                 st.insert(it);
             }
         }
 
         return minDel;
+
     }
 };
