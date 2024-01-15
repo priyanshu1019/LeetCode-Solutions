3803cf848a9340f0fa6605214519f0eec82d928a
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-01-15 15:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (89.37%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    int dp[21][21];
     bool helper(int i , int j , string &s , string &p )
     {
         if( i>=s.size() and j >= p.size() ) return true;
         if( j>= p.size()) return false;
-
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
         bool match = i < s.size() and (s[i] == p[j] or p[j] == '.');
 
         if( j+1 < p.size() and p[j+1] == '*')
         {
-            return helper(i  , j+ 2, s , p) or (match and helper(i+1 , j , s , p));
+            return dp[i][j] = helper(i  , j+ 2, s , p) or (match and helper(i+1 , j , s , p));
         }
         if(match)
         {
-            return helper(i+1 , j+1 , s , p);
+            return dp[i][j] = helper(i+1 , j+1 , s , p);
         }
-        return false;
+        return dp[i][j] = false;
 
     }
     bool isMatch(string s, string p) {
+        memset(dp , -1 , sizeof(dp));
        return  helper(0,0,s,p);
     }
 };
