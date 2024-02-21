bf82deb0bbf3fc0cd37b244b26624624bb9a4b4e
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-02-21 19:33:06 +0530

    Time: 4 ms (57.44%) | Memory: 14.5 MB (32.23%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
-        int n   = nums.size();
+        int n = nums.size();
         int ind = -1;
-        
-        //break point
-        for(int i = n-2 ; i >=0 ; i--){
-            
-            if( nums[i+1] > nums[i] ){
-                
+        for(int i = n-2;i>=0 ; i--)
+        {
+            if( nums[i+1] > nums[i])
+            {
                 ind = i;
                 break;
-                
             }
-            
         }
-        cout<<ind;
-        if( ind == -1){
-            reverse(nums.begin()  , nums.end());
-            return;
+        if( ind == -1)
+        {
+            reverse(nums.begin() , nums.end());
+            return ;
         }
-        cout<<"here";
-        //find just greater element and swap
-        for(int i = n-1; i > ind ; i-- ){
-            
-            if(nums[i] > nums[ind]){
-                swap(nums[i] , nums[ind]);
+        //find just greatest and swap
+        for(int i = n-1; i>ind ; i--)
+        {
+            if( nums[i] > nums[ind])
+            {
+                swap(nums[i], nums[ind]);
                 break;
             }
-            
         }
-        
-        //reverse the elements after the break point
-        reverse(nums.begin()+ind +1, nums.end());
-        return;
-
+        reverse(nums.begin()+ind+1 , nums.end());
+        return ;
     }
 };
