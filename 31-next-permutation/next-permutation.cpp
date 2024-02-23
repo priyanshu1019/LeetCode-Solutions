a731ba312d2d9ce6fe2d6c90605e72f74d5973ab
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-02-23 16:14:39 +0530

    Time: 4 ms (57.50%) | Memory: 14.7 MB (11.67%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
         int ind = -1;
-        for(int i = n-2;i>=0 ; i--)
+        int n = nums.size();
+        for(int i = n -2 ; i  >= 0 ; i-- )
         {
             if( nums[i+1] > nums[i])
             {
@@ -12,20 +12,19 @@ public:
             }
         }
         if( ind == -1)
-        {
-            reverse(nums.begin() , nums.end());
-            return ;
+        { 
+            reverse(nums.begin(), nums.end());
+            return;
         }
-        //find just greatest and swap
-        for(int i = n-1; i>ind ; i--)
+        for(int i = n-1; i> ind ; i--)
         {
-            if( nums[i] > nums[ind])
+            if( nums[i ] > nums[ind])
             {
-                swap(nums[i], nums[ind]);
+                swap(nums[i] , nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1 , nums.end());
-        return ;
+        reverse(nums.begin()+ind+1,nums.end());
+        return;
     }
 };
