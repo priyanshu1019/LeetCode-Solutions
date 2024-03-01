4d42cb334c6ce77576e7dd5e63e8a9c03851ab5c
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-03-01 18:40:31 +0530

    Time: 7 ms (64.71%) | Memory: 48.4 MB (12.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,35 @@
+public class Solution {
+    private String generateKey(String word) {
+        int[] freq = new int[26];
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        StringBuilder key = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                char ch = (char) ('a' + i);
+                for (int j = 0; j < freq[i]; j++) {
+                    key.append(ch);
+                }
+            }
+        }
+        return key.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        int n = strs.length;
+        for (int i = 0; i < n; i++) {
+            String key = generateKey(strs[i]);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(strs[i]);
+        }
+
+        for (List<String> group : map.values()) {
+            result.add(group);
+        }
+        return result;
+    }
+
+}
