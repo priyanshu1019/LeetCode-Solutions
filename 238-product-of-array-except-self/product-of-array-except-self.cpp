a23815205ea45ee61b48061b6112f9e162c9f0a7
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-03-15 16:19:58 +0530

    Time: 11 ms (90.04%) | Memory: 26.4 MB (72.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n, 1); // Initialize left with size n and all elements as 1
-        vector<int> right(n, 1); // Initialize right with size n and all elements as 1
         
-        for(int i = 1 ; i < n ; i++ ) {
-            left[i] = nums[i - 1] * left[i - 1];
+        int n  = nums.size();
+        vector<int> answer(n);
+        int count_zero = 0;
+        int product_nonZero=1;
+        int product = 1;
+
+        for(auto it: nums)
+        {
+            product *= it;
+            if( it == 0)
+            {
+                count_zero++;
+            }else{
+                product_nonZero *= it;
+            }
         }
-        
-        for(int i = n - 2; i >= 0 ; i--) {
-            right[i] = nums[i + 1] * right[i + 1];
+
+        if( product != 0)
+        {
+            for(int i = 0 ; i<n ;i++)
+            {
+                answer[i] =product/nums[i];
+            }
         }
-        
-        vector<int> ans;
-        for(int i = 0; i < n; i++) {
-            ans.push_back(left[i] * right[i]);
+        else if( count_zero == 1){
+            for(int i = 0; i<n ; i++)
+            {
+                if( nums[i] ==0)
+                {
+                    answer[i] = product_nonZero;
+                }
+            }
         }
-        return ans;
+        return answer;
     }
 };
