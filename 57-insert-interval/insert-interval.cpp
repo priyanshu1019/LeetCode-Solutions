e1d85336177307ff00068580d110d741383a992a
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-03-17 11:49:41 +0530

    Time: 14 ms (38.68%) | Memory: 20.3 MB (82.99%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
         intervals.push_back(newInterval);
         int n = intervals.size();
-        if( n == 1) return intervals;
+        if( n== 1 ) return intervals;
         int ind = n-1;
-        while(ind >0 and intervals[ind-1][0] >= intervals[ind][0])
+
+        while(ind > 0 and intervals[ind-1][0] >= intervals[ind][0])
         {
-            if( intervals[ind-1][0] == intervals[ind][0])
+            if( intervals[ind-1][0] == intervals[ind][0] )
             {
-                if(intervals[ind-1][1] > intervals[ind][1])
+                if( intervals[ind-1][1] > intervals[ind-1][1])
                 {
-                    swap(intervals[ind-1],intervals[ind]);
+                    swap(intervals[ind], intervals[ind-1]);
                 }
             }else{
-                swap(intervals[ind-1], intervals[ind]);
+                swap(intervals[ind], intervals[ind-1]);
             }
             ind--;
         }
-        
         vector<vector<int>> merged;
-
         merged.push_back(intervals[0]);
         ind = 0;
-        for(int i = 1 ; i< n ; i++){
+        for(int i = 1 ; i<n ; i++ )
+        {
             if( intervals[i][0] <= merged[ind][1] )
             {
-                //doubt
-                merged[ind][1] = max(merged[ind][1],intervals[i][1]);
-
+                merged[ind][1] = max(merged[ind][1] , intervals[i][1]);
             }else{
                 merged.push_back(intervals[i]);
                 ind++;
             }
         }
+
         return merged;
     }
 };
