a710e7544391f0dc7e25193081d33ec2914cbd2d
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-08 12:39:49 +0530

    Time: 33 ms (24.05%) | Memory: 23.7 MB (12.11%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,61 @@
 class Solution {
 private:
-    void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&visited){
-        int n=grid.size();
-        int m=grid[0].size();
-        visited[row][col]=1;
+
+    void bfs(int row , int col , vector<vector<int>>& vis , vector<vector<char>> &grid)
+    {
+        vis[row][col] = 1;
         queue<pair<int,int>> q;
-        q.push({row,col});
-        while(!q.empty()){
-            auto fNode=q.front();
-        int x=fNode.first;
-        int y=fNode.second;
-        q.pop();
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int newX=x+dx[i];
-            int newY=y+dy[i];
-            if((newX>=0&&newX<grid.size())
-            &&(newY>=0&&newY<grid[0].size())
-            &&!visited[newX][newY]
-            &&grid[newX][newY]=='1')
+        q.push({row , col});
+        int n  = grid.size();
+        int m  = grid[0].size();
+        while(!q.empty())
+        {
+            auto currNode = q.front();
+            q.pop();
+
+            int currRow = currNode.first;
+            int currCol = currNode.second;
+
+            int dx[] = {-1 , 0 , 1 , 0};
+            int dy[] = {0 , 1 , 0 , -1};
+
+            for(int i = 0 ; i < 4 ; i++)
             {
-                q.push({newX,newY});
-                visited[newX][newY]=true;
+                int newX = currRow+dx[i];
+                int newY = currCol+dy[i];
+
+                if( (newX >= 0 and newX < n ) && (newY >=0 and newY <m ) 
+                && grid[newX][newY] == '1' && !vis[newX][newY] )
+                {
+                    vis[newX][newY] = 1;
+                    q.push({newX , newY});
+                }
             }
-        }
         }
     }
+
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        //creating our 2d visited array
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        int numOfIslands=0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-                if(!visited[row][col] && grid[row][col]=='1'){
-                    numOfIslands++;
-                    bfs(row,col,grid,visited);
-                }
+        
+        int count = 0;
+        int n     = grid.size();
+        int m     = grid[0].size();
+        
+        vector<vector<int>> vis( n , vector<int>(m , 0));
+
+        for(int row = 0 ; row<n ; row++)
+        {
+            for(int col = 0 ; col < m ;col++)
+            {
+                if( grid[row][col] == '1' and !vis[row][col]  )
+                {
+                    count++;
+                    bfs(row ,col , vis , grid);
                 }
             }
-        return numOfIslands;
+        }
+
+        //return result
+        return count;
     }
 };
