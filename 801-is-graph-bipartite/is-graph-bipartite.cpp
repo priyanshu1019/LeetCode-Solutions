efbe5e1ad459f6af7a97509454070c7f6d3edc45
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-09 18:20:50 +0530

    Time: 19 ms (46.68%) | Memory: 18.7 MB (9.40%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,31 +1,54 @@
 class Solution {
-    bool check(int node,int n,vector<vector<int>> &graph,vector<int> &color){
-        
-       for(auto adjacent:graph[node]){
-                if(color[adjacent]==-1){
-                    color[adjacent]=!color[node];
-                    if(!check(adjacent,n,graph,color)){
-                        return false;
-                    }
-                }else if(color[adjacent]==color[node]){
+private:
+    bool check(int node , vector<int> adj[] , vector<int> &color )
+    {
+        for(int &adjNode:adj[node])
+        {
+            if( color[adjNode]  == -1)
+            {
+                color[adjNode] = !color[node];
+                if(check(adjNode , adj , color) ==false)
+                {
                     return false;
                 }
+            }else if( color[adjNode] == color[node])
+            {
+                return false;
             }
+        }
+
         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,-1);
-        for(int i=0;i<n;i++){
-            if(color[i]==-1)//not colored yet
+        int n = graph.size();
+        vector<int> adj[n];
+        vector<int> color(n , -1);
+
+        //adjList
+        for(int i = 0 ; i<n ; i++)
+        {
+            for(int j = 0 ; j<graph[i].size(); j++)
             {
-                color[i]=0;
-                if(check(i,n,graph,color)==false){
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+
+        for(int i = 0 ; i< n ; i++)
+        {
+            if( color[i] == -1)
+            {
+                //not colored
+                color[i] = 1;
+                if( check(i , adj , color) == false)
+                {
                     return false;
                 }
             }
         }
+
         return true;
+
+        
     }
 };
