e0f1afd4543ca7590bb5fd04d1bff94063e98d93
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-09 11:36:32 +0530

    Time: 66 ms (19.91%) | Memory: 37.8 MB (49.00%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,40 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        //ready with the matrix
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dist(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!mat[i][j]){
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n , vector<int>(m ,0));
+        vector<vector<int>> dist(n , vector<int>(m , 0));
+
+        queue<pair<int , pair<int,int>>> q;
+
+        for(int i =0 ; i< n ; i++)
+        {
+            for(int j = 0 ; j<m ; j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    q.push({0 , { i , j}});
+                    vis[i][j] = 1;
                 }
             }
         }
-        //now my queue has all the ones with there initial distance 0
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int d=q.front().second;
+
+        int dx[] = {-1 , 0 , 1, 0};
+        int dy[] = {0 , 1, 0 , -1};
+
+        while(!q.empty())
+        {
+            auto node = q.front();
             q.pop();
-            dist[row][col]=d;
-            //now i want to traverse the neighbours
-            for(int i=0;i<4;i++){
-                int nrow=row+dx[i];
-                int ncol=col+dy[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},d+1});
+            int dis = node.first;
+            int x   = node.second.first;
+            int y   = node.second.second;
+
+            dist[x][y] = dis;
+            for(int i =0 ; i<4 ; i++)
+            {
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+
+                if( (nx >= 0 and nx < n ) && (ny >= 0 and ny < m) 
+                && !vis[nx][ny] && mat[nx][ny] == 1 )
+                {
+
+                    vis[nx][ny] = 1;
+                    q.push({dis+1 , {nx , ny}});
                 }
             }
         }
+
         return dist;
     }
 };
