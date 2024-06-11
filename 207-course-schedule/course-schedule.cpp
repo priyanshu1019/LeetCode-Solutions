53645255f543f4d005706dd9fb8a112af752034b
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-11 08:45:15 +0530

    Time: 15 ms (70.21%) | Memory: 16.6 MB (95.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        int n=numCourses;
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(n , 0 );
         vector<int> adj[n];
-        vector<int> indegree(n,0);
-        for(auto it:prerequisites){
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
+        for(auto &vec : prerequisites)
+        {
+            int u = vec[0];
+            int v = vec[1];
+            adj[v].push_back(u);
+            indegree[u]++;
         }
 
+        int count = 0;
         queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
+        for(int i =0 ; i< n ; i++)
+        {
+            if(indegree[i]==0)
+            {
                 q.push(i);
             }
         }
 
-        int count=0;
-        while(!q.empty()){
-            int node=q.front();
+        while(!q.empty())
+        {
+            int node = q.front();
             q.pop();
             count++;
-            for(auto adjNode:adj[node]){
+
+            for(int adjNode:adj[node])
+            {
                 indegree[adjNode]--;
-                if(indegree[adjNode]==0){
+                if(indegree[adjNode]==0)
+                {
                     q.push(adjNode);
                 }
             }
         }
-        return count==n? true:false;
 
+        return count==n;
     }
 };
