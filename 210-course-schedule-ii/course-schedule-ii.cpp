25c686ff06ab0b2d70af04328bb2d4a5eaed2c3b
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-11 08:55:48 +0530

    Time: 12 ms (83.99%) | Memory: 16.7 MB (94.35%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
-        vector<int> adj[V];
-	    for(auto it:prerequisites){
-	        adj[it[1]].push_back(it[0]);
-	    }
-        vector<int> indegree(V,0);
-        for(int i=0;i<V;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n];
+        vector<int> indegree(n , 0);
+
+        for(auto &vec:pre)
+        {
+            int u = vec[0];
+            int v = vec[1];
+            adj[v].push_back(u);
+
+            indegree[u]++;
         }
         queue<int> q;
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
+        for(int i =0 ; i< n ;i++)
+        {
+            if(indegree[i] == 0 )
+            {
                 q.push(i);
             }
-            
         }
-        vector<int> ans;
-        // int count=0;
-        while(!q.empty()){
-            int node=q.front();
+
+        vector<int> order;
+
+        while(!q.empty())
+        {
+            int node = q.front();
             q.pop();
-            ans.push_back(node);
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(!indegree[it]){
-                    q.push(it);
+            order.push_back(node);
+
+            for(int &adjNode:adj[node])
+            {
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0 )
+                {
+                    q.push(adjNode);
                 }
             }
         }
-        if(ans.size()==V)return ans;
-        return {};
+        vector<int> empty;
+        return order.size() < n ? empty:order;
     }
 };
