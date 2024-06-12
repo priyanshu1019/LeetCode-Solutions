0da335ca3d3dac6a977e5623edc740013ac1ef05
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-12 07:31:28 +0530

    Time: 62 ms (34.87%) | Memory: 23.3 MB (75.91%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]==1){
+        int n = grid.size();
+        if( n == 0 || grid[0][0] != 0)
+        {
             return -1;
         }
 
-        int n=grid.size();
-        if(grid[n-1][n-1]!=0){
-            return -1;
-        }
-        queue<pair<int,pair<int,int>>> pq;
-        pq.push({1,{0,0}});
-        // grid[0,0]
-        vector<vector<int>> dist(n,vector<int>(n,1e9));
-        dist[0][0]=0;
-        int dx[] = {-1, 0, +1, 0, 1, -1, -1, 1};
-        int dy[] = {0, 1, 0, -1, 1, 1, -1, -1}; 
-        while(!pq.empty()){
-            auto it=pq.front();
-            int dis=it.first;
-            int x=it.second.first;
-            int y=it.second.second;
-            
+        auto isSafe = [&](int x , int y)
+        {
+            return x>=0 and x<n and y<n and y>=0;
+        };
+        priority_queue<pair<int,pair<int,int>> , vector<pair<int , pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0 , { 0 , 0}});
+
+        int dx[] = {-1 , 0 , 1, 0  , 1 , 1 , -1 , -1};
+        int dy[] = {0 , 1 , 0 , -1 , 1 , -1 , -1 , 1};
+        vector<vector<int>> dist( n , vector<int>( n , INT_MAX));
+        dist[0][0] = 0;
+        while(!pq.empty())
+        {
+            int dis = pq.top().first;
+            int x   = pq.top().second.first;
+            int y   = pq.top().second.second;
             pq.pop();
-            if(x==n-1 && y==n-1){
-                return dis;
-            }
-            //traverse karna hai 
-           for(int i=0;i<8;i++){
-                int newX=x+dx[i];
-                int newY=y+dy[i];
-                if(newX>=0 && newX<n && newY>=0 && newY<n && grid[newX][newY]==0 && dis+1<dist[newX][newY]){
-                    dist[newX][newY]=dis+1;
-                    pq.push({dist[newX][newY],{newX,newY}});
+
+            if( x == n-1 and y == n-1) //it means we are at our dest
+                return dis+1;
+            
+            for(int i =0; i<8 ; i++)
+            {
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if( isSafe(nx , ny) && grid[nx][ny] == 0 && dis+1<dist[nx][ny])
+                {
+                    dist[nx][ny] = 1+dis;
+                    pq.push({dist[nx][ny] , {nx , ny}});
                 }
-           }
+            }
         }
+
         return -1;
     }
 };
