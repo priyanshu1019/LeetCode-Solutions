c1149ebc5767c550bb5a528819bbade6a62aa34e
Priyanshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-12 08:15:28 +0530

    Time: 87 ms (68.05%) | Memory: 23.4 MB (88.38%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        int n = heights.size();
+        int m = heights[0].size();
+        auto isSafe = [&](int x , int y)
+        {
+            return x>=0 and x<n and y>=0 and y<m;
+        };
+        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
         pq.push({0,{0,0}});
-        dist[0][0]=0;
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        while(!pq.empty()){
-            auto it =pq.top();
+
+        vector<vector<int>> effort(n , vector<int>( m , 1e9));
+        effort[0][0] = 0;
+        int dx [] = {-1 , 0 , 1, 0};
+        int dy [] = {0 , 1, 0 , -1};
+        while(!pq.empty())
+        {
+            int diff = pq.top().first;
+            int x   = pq.top().second.first;
+            int y   = pq.top().second.second;
             pq.pop();
-            int dif=it.first;
-            int x=it.second.first;
-            int y=it.second.second;
-            if(x==n-1 && y==m-1) return dif;
-            for(int i=0;i<4;i++){
-                int newX=x+dx[i];
-                int newY=y+dy[i];
-                if(newX>=0 && newX<n && newY>=0 && newY<m){
-                    int newEffort=max(dif,abs(heights[x][y]-heights[newX][newY]));
-                        if(newEffort<dist[newX][newY]){
-                            dist[newX][newY]=newEffort;
-                            pq.push({newEffort,{newX,newY}});
-                        }
-                    
+
+            if( x == n-1 and y == m-1 )
+            {
+                return diff;
+            }
+
+            for(int i = 0 ; i< 4 ; i++)
+            {
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                if( isSafe(nx , ny))
+                {
+                    int newEffort = max(diff , abs(heights[x][y] - heights[nx][ny]));
+                    if(newEffort < effort[nx][ny])
+                    {
+                        effort[nx][ny] = newEffort;
+                        pq.push({newEffort , {nx , ny}});
+                    }
                 }
             }
         }
-        return 0;
+
+        return -1;
     }
 };
