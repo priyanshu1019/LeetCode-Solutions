6e99f93d3eaccb33660128ca0f7c304fb5831708
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-22 12:51:35 +0530

    Time: 13 ms (49.00%) | Memory: 20.5 MB (74.47%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -3,36 +3,30 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         intervals.push_back(newInterval);
         int n = intervals.size();
-        if( n== 1 ) return intervals;
         int ind = n-1;
 
-        while(ind > 0 and intervals[ind-1][0] >= intervals[ind][0])
-        {
-            if( intervals[ind-1][0] == intervals[ind][0] )
-            {
-                if( intervals[ind-1][1] > intervals[ind-1][1])
-                {
-                    swap(intervals[ind], intervals[ind-1]);
+        while( ind > 0 and intervals[ind-1][0] >= intervals[ind][0]){
+            if( intervals[ind-1][0] == intervals[ind][0] ){
+                if( intervals[ind-1][1] > intervals[ind][1]){
+                    swap(intervals[ind-1] , intervals[ind]);
                 }
             }else{
-                swap(intervals[ind], intervals[ind-1]);
+                swap(intervals[ind-1] , intervals[ind]);
             }
             ind--;
         }
-        vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
-        ind = 0;
-        for(int i = 1 ; i<n ; i++ )
-        {
-            if( intervals[i][0] <= merged[ind][1] )
-            {
-                merged[ind][1] = max(merged[ind][1] , intervals[i][1]);
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+
+        for(int i = 1 ; i < n ; i++){
+            if( result.back()[1] >= intervals[i][0]){
+                result.back()[1] = max(result.back()[1] , intervals[i][1]);
             }else{
-                merged.push_back(intervals[i]);
-                ind++;
+                result.push_back(intervals[i]);
             }
         }
 
-        return merged;
+        return result;
+
     }
 };
