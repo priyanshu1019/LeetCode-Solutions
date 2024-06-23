5c962c208484fc01b69ad2948e35079e2617e5d9
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-06-24 00:10:46 +0530

    Time: 6 ms (77.92%) | Memory: 9.2 MB (88.11%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int calculate(string s) {
-        int n=s.size();
-        int number=0;
-        int result=0;
-        int sign=1;
         stack<int> st;
-        for(int i=0;i<n;i++ ){
-            if(isdigit(s[i])){
-                number=number*10+(s[i]-'0');
-            }else if(s[i]=='+'){
+        int sign = 1;
+        int result = 0 ;
+        int number = 0;
+        int n      = s.size();
 
-                result+=(sign*number);
-                number=0;
-                sign=1;
-
-            }else if(s[i]=='-'){
-                result+=(sign*number);
-                number=0;
-                sign= -1;
-            }else if(s[i]=='('){
-
+        for(int i =0 ; i < n ; i++){
+            if( isdigit(s[i])){
+                number = number*10 + (s[i]-'0');
+            }else if( s[i] == '+'){
+                result += (sign * number);
+                sign = 1;
+                number = 0;
+            }else if( s[i] == '-'){
+                result += (sign * number);
+                sign = -1;
+                number = 0;
+            }else if( s[i] == '('){
                 st.push(result);
                 st.push(sign);
-                result=0;
-                number=0;
-                sign=1;
-            }else if(s[i]==')'){
-               result += sign*number;
+                sign = 1;
                 number = 0;
-                int top = st.top(); st.pop();
-                result *= top;
-                top = st.top(); st.pop();
-                result += top;
+                result = 0;
+            }else if( s[i] == ')'){
+                //closing brackets
+                result += (number*sign);
+                number = 0;
+
+                int stack_sign = st.top();st.pop();
+                int last_val   = st.top();st.pop();
+                result = last_val + (stack_sign*result);
             }
         }
-        result+=sign*number;
+
+        //last wali value toh result me aaya hi nahi bhai
+        result +=(sign*number);
+
         return result;
     }
 };
