00d456558a3cf0fbfc4b504eeab354e07d642184
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-15 13:18:00 +0530

    Time: 8 ms (76.80%) | Memory: 16.9 MB (7.49%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-
         int n = matrix.size();
         int m = matrix[0].size();
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                if (matrix[row][col] == 0) {
 
-                    for (int k = 0; k < m; k++) {
-                        if (k != col && matrix[row][k] != 0) {
-                            matrix[row][k] = -1e9;
+        for(int row = 0 ; row<n ; row++){
+            for(int col =0 ; col < m ; col++){
+                if( matrix[row][col] == 0){
+                    for(int k = 0 ; k < m ; k++){
+                        if( k != col and matrix[row][k] != 0){
+                            matrix[row][k] =1e9;
                         }
                     }
-                    for (int k = 0; k < n; k++) {
-                        if (k != row && matrix[k][col] != 0) {
-                            matrix[k][col] = -1e9;
+
+                    for(int k = 0 ; k < n ; k++){
+                        if( k!= row and matrix[k][col] != 0){
+                            matrix[k][col] = 1e9;
                         }
                     }
                 }
             }
         }
 
-        //make all zero 
-        for( int row = 0 ; row<n ; row++)
-        {
-            for(int  col = 0 ; col < m ; col ++)
-            {
-                if( matrix[row][col]  == -1e9 )
-                {
-                    matrix[row][col]  = 0;
+        for(int row = 0; row < n ; row++ ){
+            for(int col =0 ; col < m ; col++ ){
+                if(matrix[row][col] == 1e9){
+                    matrix[row][col] =0;
                 }
             }
         }
-
-
     }
 };
