5c3b13e4a52c8a502a83955afeec2bed7e4e2754
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-15 13:22:11 +0530

    Time: 11 ms (90.57%) | Memory: 21.8 MB (15.52%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -6,16 +6,11 @@ public:
 
         for(int j = 0 ; j< n ; j++)
         {
-            if( j == 0 and nums[j] != 0)
-            {
-                i++;
-            }
-            if(j != 0 and nums[j] != 0)
+            if(nums[j] != 0)
             {
                 nums[++i] = nums[j];
             } 
         }
-        cout<<i<<endl;
         if(i ==-1 || i == n) return ;  
         for(int j = i+1 ; j< n ; j++)
         {
