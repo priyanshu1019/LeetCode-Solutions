181de4238cef96742a984964971eacb12f9234cc
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-16 23:35:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (5.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -3,8 +3,9 @@ public:
     void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
         int left = n-1;
         int right = 0;
-        while( left >= 0 and right < m){
-            if( nums1[left] <= nums2[right]){
+
+        while(left >=0 and right < m ){
+            if(nums1[left] <= nums2[right]){
                 break;
             }else{
                 swap(nums1[left--] , nums2[right++]);
@@ -13,10 +14,10 @@ public:
 
         sort(nums1.begin() , nums1.begin()+n);
         sort(nums2.begin() , nums2.end());
+
         int i = n;
-        for(int j = 0 ; j < m ; j++){
+        for(int j = 0 ; j < m ; j++ ){
             nums1[i+j] = nums2[j];
         }
-
     }
 };
