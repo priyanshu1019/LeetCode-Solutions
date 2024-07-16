6b501d920b3da43e173436861544a6c072b06161
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-16 11:02:38 +0530

    Time: 40 ms (65.86%) | Memory: 45 MB (9.03%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int maxScore(vector<int>& points, int k) {
-        int n = points.size();
-        if( k == n ) return accumulate(points.begin() , points.end(), 0);
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        if( n == k) return accumulate(cardPoints.begin() , cardPoints.end(),0);
+
         int score = 0;
-        int maxScore = 0;
-        int l , r;
-        for(int i = 0 ; i< k ; i++)
-        {
-            score += points[i];
+        int maxScore = INT_MIN;
+        for(int i =0 ; i < k ; i++){
+            score+= cardPoints[i];
         }
         maxScore = score;
-        l = 0;
-        r = k-1;
+        int l = 0 , r = k-1;
 
-        while(l != n-k)
-        {
-            score -= points[r];
-            l = (l-1 + n) %n;
-            r = (r-1 + n) % n;
-            score += points[l];
-            maxScore = max(maxScore , score);
-            cout<<"l "<<l<<" r "<<r <<" score "<<score<<" maxSCore "<<maxScore<<endl;
+        while( l != n-k){
+            score -= cardPoints[r];
+            l  = (l-1+n)%n;
+            r  = (r-1+n)%n;
+            score += cardPoints[l];
+            maxScore = max(score,maxScore);
         }
 
         return maxScore;
