01b5f74b6555b4a0def50351ce3cad074e5b9d46
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-16 23:51:48 +0530

    Time: 56 ms (22.85%) | Memory: 51.2 MB (20.22%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        if( n == 0 || n == 1 ) return true; 
-        //this is the point till what i can jump definitely 
+        if( n == 0 || n == 1)return true;
         int maxInd = 0;
-
-        for(int i = 0 ; i < n ;i++ ){
-            if( i > maxInd) return false;
+        for(int i = 0 ; i < n ; i++){
+            if( i > maxInd){
+                return false;
+            }
             maxInd = max(maxInd , i+nums[i]);
         }
-
-        return true;
-
         return true;
     }
 };
