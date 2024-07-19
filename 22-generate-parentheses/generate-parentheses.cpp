4af36e53e67c144c0afd45cfd73335c674ab7bab
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-19 09:20:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (80.98%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,40 +1,33 @@
 class Solution {
 private:
-    bool isValid( string &str){
-        stack<char> st;
-
-        for(char ch : str){
-            if( ch == '('){
-                st.push(ch);
-            }else{
-                if( st.empty() || st.top() != '(' and ch == ')') return false;
-                st.pop();
-            }
-        }
-        return st.empty();
-    }
-    void solve(string &str , vector<string>&result , int n ){
+    
+    void solve(string &str , vector<string>&result , int n , int open , int close){
         if( str.size() == 2*n) {
-            if( isValid(str) == true) result.push_back(str);
-
+            result.push_back(str);
             return;
         }
 
         //open bracket 
-        str.push_back('(');
-        solve(str , result , n);
+        if( open < n){
+            str.push_back('(');
+            solve(str , result , n , open +1, close);
+            str.pop_back();
+        }
         //backtrack
-        str.pop_back();
         //close bracket
-        str.push_back(')');
-        solve(str , result , n);
-        str.pop_back();
+        if( close < open){
+            str.push_back(')');
+            solve(str , result , n , open , close+1);
+            str.pop_back();
+        }
+        
     }
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
         string str ="";
-        solve(str, result , n );
+
+        solve(str, result , n , 0 , 0 );
 
         return result;
     }
