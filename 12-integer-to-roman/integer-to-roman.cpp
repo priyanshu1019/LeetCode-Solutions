6a9085f8c64119248b0ec608b9dfb564935a6838
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-19 08:00:05 +0530

    Time: 71 ms (5.39%) | Memory: 154.9 MB (5.46%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string roman = "";
+        string result = "";
         vector<string> mp(1001 , "");
-        mp[1] = 'I';
+        mp[1] = "I";
         mp[4] = "IV";
-        mp[5] = 'V';
+        mp[5] = "V";
         mp[9] = "IX";
-        mp[10] = 'X';
-        mp[40] ="XL";
-        mp[50] = 'L';
+        mp[10] = "X";
+        mp[40] = "XL";
+        mp[50] = "L";
         mp[90] = "XC";
-        mp[100] = 'C';
+        mp[100] = "C";
         mp[400] = "CD";
-        mp[500] = 'D';
+        mp[500] = "D";
         mp[900] = "CM";
-        mp[1000] = 'M';
-        for(int i = 1000 ; i>0 ; i--)
-        {
-                if(mp[i] == "") continue;
-                int temp = num/i;
-                for(int j = 0 ; j <temp ; j++)
-                {
-                    roman += mp[i];
-                }
-                num = num % i;
-            
-            if( num == 0)
-            {
-                break;
+        mp[1000] = "M";
+
+        for(int i =1000 ; i >= 1 ; i--){
+            if( mp[i] == ""){
+                continue;
             }
+            int times = num  / i;
+            for(int j = 0 ; j < times ; j++){
+                result += mp[i];
+            }
+            num = num % i;
+            if( num == 0 ) break;
         }
 
-        return roman;
+        return result;
     }
 };
