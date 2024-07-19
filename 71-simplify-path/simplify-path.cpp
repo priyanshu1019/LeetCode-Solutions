7f125ecd86d648bed3728776a65deb2cb0d32b9c
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-19 09:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (11.55%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string token="";
-        stringstream ss(path);
+        string result = "";
+        string token  = "";
         stack<string> st;
-        while(getline(ss,token,'/')){
-            if(token==""|| token=="."){
+        stringstream ss(path);
+        
+        while(getline(ss , token , '/')){
+            if( token == "" || token == "."){
                 continue;
+            }else if( token == ".."){
+                if( !st.empty()) st.pop();
+            }else{
+                st.push(token);
             }
-                if(token!=".."){
-                    //some folder name
-                    st.push(token);
-                }else if(!st.empty()){
-                    //.. so pop
-                    st.pop();
-                }
-            
         }
-        string result="";
-        if(st.empty()){
-            return "/";
-        }
+        if( st.empty())return "/";
         while(!st.empty()){
-            result="/"+st.top()+result;
+            result ="/"+st.top()+ result;
             st.pop();
         }
+
         return result;
     }
 };
