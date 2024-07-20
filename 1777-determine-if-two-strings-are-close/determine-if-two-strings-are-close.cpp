8ee0703372542e261322637cbf655c17d4e58be7
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-20 09:48:43 +0530

    Time: 70 ms (58.83%) | Memory: 22.1 MB (58.98%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        map<char , int> mp1 , mp2;
-        for(auto it:word1)
-        {
-            mp1[it]++;
-        }
-        for(auto it:word2)
-        {
-            mp2[it]++;
-        }
-        map<int, int> mp;
-        for(auto it:mp1)
-{
-    if(mp2.find(it.first) == mp2.end()) return false;
-    mp[it.second]++;
-}
-for(auto it:mp2)
-{
-    if(mp1.find(it.first) == mp1.end()) return false;
-    mp[it.second]--;
-}
+        int n = word1.size();
+        int m = word2.size();
+        if( n!= m)return false;
 
-        for(auto it:mp)
-        {
-            if(it.second != 0)
-            {
-                return false;
-            }
+        vector<int> freq1(26  ,0) , freq2(26 , 0);
+
+        for(int i =0 ; i < n ; i++){
+            int ind1 = word1[i]-'a';
+            int ind2 = word2[i]-'a';
+
+            freq1[ind1]++;
+            freq2[ind2]++;
         }
-        return true;
+        //first point
+        for(int i =0 ; i < 26 ;i++){
+            if( freq1[i]!= 0 and freq2[i]!= 0) continue;
+            if( freq1[i] == 0 and freq2[i] == 0) continue;
+            return false;
+        }
+
+        //second point;
+        sort(freq1.begin() , freq1.end());
+        sort(freq2.begin() , freq2.end());
+
+        return freq1 == freq2;
     }
 };
