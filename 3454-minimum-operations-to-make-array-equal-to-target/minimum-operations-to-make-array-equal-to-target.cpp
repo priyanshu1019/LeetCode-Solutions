e23f1611c2800831eabc256020405275d6277959
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-21 16:12:29 +0530

    Time: 109 ms (100.00%) | Memory: 122.2 MB (100.00%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -3,15 +3,10 @@ public:
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
         long long result = 0;
         int n = nums.size();
-        vector<int> diff(n);
-
-        for(int i = 0 ;i < n ; i++){
-            diff[i] = target[i] - nums[i];
-        }  
         int prev = 0;
         int curr = 0;
         for(int i =0 ; i < n; i++){
-            curr = diff[i];
+            curr = target[i]-nums[i];
             //sign change
             if((curr > 0 and prev < 0)||( curr < 0 and prev > 0)){
                 result += abs(curr);
