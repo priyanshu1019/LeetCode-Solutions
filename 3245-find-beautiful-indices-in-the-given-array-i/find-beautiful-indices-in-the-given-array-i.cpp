bab3418cefdda3c52a946ff2d6ddeb18cb61a899
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-23 11:46:27 +0530

    Time: 25 ms (91.07%) | Memory: 18.2 MB (74.21%) - LeetSync

---
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    void computeLps(string &pat , vector<int>& lps){
+        lps[0]  = 0;
+        int len = 0;
+        int i   = 1;
+        int M   = pat.length();
+
+        while( i < M ){
+            if( pat[i]  == pat[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if( len!= 0){
+                    len = lps[len-1]; 
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    vector<int> KMP(string &txt , string &pat ){
+        int N = txt.length();
+        int M = pat.length();
+        int i = 0;
+        int j = 0;
+        vector<int> result;
+        vector<int> lps(M , 0);
+        computeLps(pat , lps);
+
+        while(i < N ){
+            if( txt[i] == pat[j]){
+                i++;
+                j++;
+            }
+            if( j == M){
+                result.push_back(i - M);
+                j = lps[j-1];
+            }else if( txt[i] != pat[j]){
+                if( j!= 0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        vector<int> result;
+        vector<int> i_indices = KMP(s , a);
+        vector<int> j_indices = KMP(s , b);
+        int n = s.length();
+        for(int &i : i_indices){
+            int leftLimit = max( 0 , i - k);
+            int rightLimit = min( n-1 , i+k);
+
+            auto it = lower_bound(begin(j_indices) , end(j_indices) , leftLimit);
+            if( it != j_indices.end() and *it <=rightLimit){
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+};
