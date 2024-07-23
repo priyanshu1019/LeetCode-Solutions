a70bec2f74514df09d561e99a86a9bc30cc30ec2
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-23 23:19:01 +0530

    Time: 2 ms (39.85%) | Memory: 9.5 MB (15.19%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,29 +1,21 @@
 class Solution {
 private:
-    int helper(int ind,vector<int> &nums,vector<int> &dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1){
-            return dp[ind];
+    int helper( vector<int>& nums ,  int ind, vector<int>&dp){
+        if( ind >= nums.size()){
+            return 0;
         }
-        int pick=nums[ind]+helper(ind-2,nums,dp);
-        int non_pick=helper(ind-1,nums,dp);
-        return dp[ind]=max(pick,non_pick);
+        if( dp[ind] != -1) return dp[ind];
+        int take = nums[ind] + helper(nums , ind+2 , dp);
+        int notTake = helper(nums , ind+1 , dp);
+
+        return dp[ind] = max(take , notTake);
+
     }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        // vector<int> dp(n+1,-1);
-        // dp[0]=nums[0];
-        int prev1=0;
-        int prev2=0;
-        for(int i=0;i<n;i++){
-            int pick=nums[i];
-            pick+=prev2;
-            int non_pick=prev1;
-            prev2=prev1;
-            prev1=max(pick,non_pick);
-        }
-        return prev1;
+        int prev = -1;
+        int n= nums.size();
+        vector<int> dp(n+1 , -1);
+        return helper( nums  , 0, dp);
     }
 };
