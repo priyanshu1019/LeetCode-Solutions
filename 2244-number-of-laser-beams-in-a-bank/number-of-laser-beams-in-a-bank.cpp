78b61944e1cdb2bb0bee4b1d8d5fe6851a7082b5
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-23 09:40:12 +0530

    Time: 96 ms (19.35%) | Memory: 27 MB (49.71%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        
-        int n = bank.size();
-        int prev  = 0;
-        int total = 0;
-        for(int i = 0 ; i < n ; i++ ){
+        int n    = bank.size();
+        int m    = bank[0].size();
+        int curr = 0;
+        int prev = 0;
+        int total= 0;
 
-            int curr = 0;
-            for(int j = 0 ; j < bank[i].size() ; j++){
-
-                if( bank[i][j] == '1' ){
+        for(int i =0 ; i < n ; i++){
+            curr = 0;
+            for(int j = 0 ; j < m ; j++){
+                if( bank[i][j] == '1'){
                     curr++;
                 }
-
             }
-            cout<< curr<<endl;
-            if(i!= 0 and curr != 0 ){
-
-                total += prev * curr; 
-
+            if( curr != 0 and i != 0){
+                total += (curr * prev);
             }
-            if( curr != 0)
+            if( curr != 0){
                 prev = curr;
-
-
-
+            }
         }
 
         return total;
-
     }
 };
