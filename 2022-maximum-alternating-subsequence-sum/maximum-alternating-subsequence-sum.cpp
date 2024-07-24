a29458464ff325618ee4111bcedadac8f0edce6a
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-24 15:26:31 +0530

    Time: 294 ms (10.25%) | Memory: 166.8 MB (5.13%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,47 +1,21 @@
 class Solution {
-private:
-    long long helper(int index, vector<int> &nums,bool even,vector<vector<long long >> &dp){
-        //base case
-        if(index==nums.size()){
-            return 0;
-        }
-        if(dp[index][even]!=-1){
-            return dp[index][even];
-        }
-        //not_pick
-        long long not_pick=helper(index+1,nums,even,dp);
-        //pick
-        long long pick=INT_MIN;
-        if(even){
-            pick=nums[index]+helper(index+1,nums,false,dp);
-        }else if(even==false){
-            pick=-nums[index]+helper(index+1,nums,true,dp);
+public: 
+    long long helper(vector<int>&nums , int ind , bool even , vector<vector<long long>>&dp){
+        if( ind == nums.size()) return 0;
+        if( dp[ind][even]!= -1) return dp[ind][even];
+        long long notPick = helper(nums , ind + 1, even , dp );
+        long long  pick    = INT_MIN;
+
+        if( even ){
+            pick    = nums[ind] + helper(nums , ind+1 , false, dp);
+        }else{
+            pick    = -nums[ind]+ helper(nums , ind+1 , true , dp);
         }
 
-        return dp[index][even]=max(not_pick,pick);
+        return dp[ind][even]=max(pick , notPick);
     }
-public:
     long long maxAlternatingSum(vector<int>& nums) {
-        vector<vector<long long >> dp(nums.size()+1,vector<long long >(2,0));
-        //base case
-        int n=nums.size();
-        dp[n][0]=0;
-        dp[n][1]=0;
-        for(int index=n-1;index>=0;index--){
-            for(int j=0;j<=1;j++){
-                //not_pick
-                long long not_pick=dp[index+1][j];
-                //pick
-                long long pick=INT_MIN;
-                if(j){
-                    pick=nums[index]+dp[index+1][!j];
-                }else if(j==false){
-                    pick=-nums[index]+dp[index+1][!j];
-                }
-
-                dp[index][j]=max(not_pick,pick);
-            }
-        }
-        return dp[0][1];
+        vector<vector<long long >> dp(nums.size()+1 , vector<long long >(2 , -1));
+        return helper(nums , 0 , true , dp);
     }
 };
