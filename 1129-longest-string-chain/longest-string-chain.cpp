94d0b57f014bf4eb598c555a4bcb93df55518943
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-25 10:20:56 +0530

    Time: 89 ms (46.55%) | Memory: 27.9 MB (41.98%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -4,8 +4,7 @@ private:
         int count = 0;
         int n = curr.size();
         int m = prev.size();
-        if( m >= n) return false;
-        if( n - m >= 2)return false;
+        if( m >= n ||  n - m >= 2) return false;
         int j = 0;
         for(int i = 0 ; i<n;){
             if( curr[i] == prev[j]){
@@ -17,18 +16,7 @@ private:
         }
         return count == 1;
     }
-    int helper(vector<string>& words , int ind , int prev , vector<vector<int>>&dp ){
-
-        if(ind >= words.size()) return 0;
-        if( dp[ind][prev+1] != -1)return dp[ind][prev+1];
-        int len = helper(words , ind+1 , prev , dp);
-
-        if( prev == -1 || isPossible(words[ind] , words[prev])){
-            len = max(len , 1+ helper(words , ind+1 , ind, dp));
-        }
-        return dp[ind][prev+1]=len;
-
-    }
+    
 public:
     int longestStrChain(vector<string>& words) {
         auto cmp = [&](string a , string b){
@@ -36,8 +24,16 @@ public:
         };
         sort(words.begin() , words.end() , cmp);
         int n = words.size();
-        vector<vector<int>> dp(n , vector<int>(n+1 , -1));
-        return helper(words , 0 , -1 , dp);
-        return 0;
+        int maxLEN = 1;
+        vector<int>dp(n , 1);
+        for(int ind = 0 ; ind < n ; ind++){
+            for(int prev = 0 ; prev < ind ; prev++){
+                if( isPossible(words[ind] , words[prev])){
+                    dp[ind] = max(dp[ind] , 1+dp[prev]);
+                }
+                maxLEN = max(maxLEN , dp[ind]);
+            }
+        }
+        return maxLEN;
     }
 };
