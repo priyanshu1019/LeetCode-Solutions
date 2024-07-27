23acf2c757c46452751bf15c56251639d7ad9266
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-27 22:44:45 +0530

    Time: 296 ms (72.72%) | Memory: 10.1 MB (72.77%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,38 +1,35 @@
 class Solution {
-public:
-    vector<vector<int>> direction = {{-1 ,0},{1,0},{0,1},{0,-1}};
-    bool find(vector<vector<char>>&board , int i , int j , int idx , string &word){
-        int n= board.size();
-        int m = board[0].size();
-        if( idx >= word.size()) return true;
+private:
+    int n , m;
+    vector<vector<int>>direction  = {{-1,0},{0,1},{0,-1},{1,0}};
+    bool find(int i , int j ,int ind, vector<vector<char>>& board , string &word){
+        if( ind == word.size())return true;
+        if( i < 0 || i >= n || j < 0 || j >= m ||board[i][j] == '$'|| board[i][j] != word[ind]) return false;
 
-        if(  i <0 || j < 0 || j>=m ||i>=n||board[i][j] != word[idx]){
-            return false;
-        }
-
-        char temp = board[i][j];
+        char temp   = board[i][j];
         board[i][j] = '$';
-        for(auto &dir:direction){
-            int new_i = i+dir[0];
-            int new_j = j+dir[1];
-            if( find(board , new_i , new_j , idx+1 , word) ){
-                return true;
-            }
-        }
+
+        for(auto &dir : direction){
+            int n_i = i+dir[0];
+            int n_j = j+dir[1];
+            if( find(n_i , n_j , ind+1 , board , word) == true)return true;
+        } 
         board[i][j] = temp;
         return false;
     }
+public:
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        if( n*m  < word.size())return false; 
-        for(int i =0 ; i < n ; i++){
+        n = board.size();
+        m = board[0].size();
+
+        for(int i = 0; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if( board[i][j] == word[0] and find(board , i , j , 0 , word)){
+                if( board[i][j] == word[0] and find(i , j ,0, board , word)){
                     return true;
                 }
             }
-        }   
+        }
+
         return false;
     }
 };
