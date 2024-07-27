1ab0b21ac2848902155ec5a9b65d2ba1ec5efc7b
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-27 09:47:30 +0530

    Time: 29 ms (95.98%) | Memory: 12.3 MB (77.12%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int t[1001][1001];
-    int LPS(string& s, int i, int j) {
-        if(i > j)
-            return 0;
-        if(i == j)
-            return 1;
+    int helper(string &s , int i  , int j ){
+        if( i > j )return 0;
+        if( i == j) return 1;
+        if( t[i][j] != -1) return t[i][j];
+        if( s[i] == s[j]){
+            return t[i][j] = 2 + helper(s , i+1 , j-1 );
+        }else
+            return t[i][j] = max(helper(s , i+1 , j) , helper(s , i , j-1));
         
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s[i] == s[j])
-            return t[i][j] = 2 + LPS(s, i+1, j-1);
-        else
-            return t[i][j] = max(LPS(s, i+1, j), LPS(s, i, j-1));
     }
-    
     int longestPalindromeSubseq(string s) {
-        int m = s.length();
-        memset(t, -1, sizeof(t));
-        
-        return LPS(s, 0, m-1); //Approach-1
-        
+        int n = s.size();
+        memset(t , -1 , sizeof(t));
+        return helper(s , 0 , n-1 );
     }
 };
