ffc2488557bb179707d909e7caeefe7455c225de
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-08-10 16:53:10 +0530

    Time: 39 ms (23.35%) | Memory: 21.7 MB (46.37%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,46 +1,30 @@
 class Solution {
-private:
-    int getTotalTime(vector<int> &piles , int h , int mid)
-    {
-        int total = 0;
-        for(int i = 0 ; i < piles.size() ; i++)
-        {
-            total += ceil((double)piles[i]/mid); //time for this pile 
-            //check if the total time has already crossed the limited time or not 
-            if( total > h)
-            {
-                //-3 indicates the above case
-                return -3;
-            }
-        }
-        return total;
-    }
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int maxi = -1e9;
-        for(int i = 0 ; i < n ; i++)
-        {
-            maxi = max(maxi , piles[i]);
+    int check(vector<int>&piles , int k , int h){
+        int hours = 0;
+        for(int i = 0 ; i < piles.size() ; i++ ){
+            hours += ceil((double)piles[i]/k);
+            if( hours > h)return -3;
         }
-        //minimum number of bananas that koko can eat in 1 hour
+        return -1;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int high = 0;
+        for(int x:piles){
+            high = max(high , x);
+        }
         int low = 1;
-        //maximum number of bananas that koko can eat in 1 hour
-        int high= maxi;
-        while(low <= high)
-        {
-            int mid = low +(high - low)/2;
-            int totalTime = getTotalTime(piles , h , mid);
-            if( totalTime == -3)
-            {
-                //we have to increase the number of bananas to eat per hour
-                low = mid + 1;
+
+        int ans = high;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int val = check(piles , mid  , h);
+            if( val == -3){
+                low = mid+1;
             }else{
-                //it can be the answer but we check for the better answer;
-                high= mid -1;
+                high = mid-1;
             }
         }
         return low;
-
     }
 };
