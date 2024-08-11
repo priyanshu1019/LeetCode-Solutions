95dd36f275c86e4b27e55dbb781e85690883aa70
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-08-11 16:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (32.35%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& vec, int k) {
-            int low = 0;
-            int n = vec.size();
-            int high = n -1;
-            while( low <= high)
-            {
-                int mid = low + (high - low)/2;
-                int missing = vec[mid] - (mid + 1);
-
-                if( missing >= k)
-                {
-                    high = mid -1;
-                }else{
-                    low = mid + 1;
-                }
+    int findKthPositive(vector<int>& arr, int k) {
+        int n   = arr.size();
+        int low = 0;
+        int high= n-1;
+        if( arr[0] > k){
+            return k;
+        }
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int missing = arr[mid] - (mid+1);
+            if( missing>= k){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
-            return k + high + 1;
+        }
+        int missing = arr[high] - (high+1);
+        return arr[high]+(k-missing);
     }
 };
